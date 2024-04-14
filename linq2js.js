0cf2895146ba1ecf6d733ba2e47457bee3a67e48
adam ketterhagen <adam@aperturelabs.biz>
2024-04-13 19:11:00 -0500

    Create linq2js.js

---
+++ linq2js.js
@@ -0,0 +1,40 @@
+Array.prototype.First = function () {
+    if (this.length === 0) {
+        throw new Error("Sequence contains no elements");
+    }
+    return this[0];
+};
+
+Array.prototype.FirstOrDefault = function (predicate) {
+    for (let i = 0; i < this.length; i++) {
+        if (!predicate || predicate(this[i])) {
+            return this[i];
+        }
+    }
+    return null;
+};
+
+Array.prototype.Last = function () {
+    if (this.length === 0) {
+        throw new Error("Sequence contains no elements");
+    }
+    return this[this.length - 1];
+};
+
+Array.prototype.LastOrDefault = function (predicate) {
+    for (let i = this.length - 1; i >= 0; i--) {
+        if (!predicate || predicate(this[i])) {
+            return this[i];
+        }
+    }
+    return null;
+};
+
+Array.prototype.Select = function (selector) {
+    return this.map(selector);
+};
+
+Array.prototype.SelectMany = function (selector) {
+    return this.flatMap(selector);
+};
+
